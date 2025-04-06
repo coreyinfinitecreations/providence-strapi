ecd2a086e6671e83f200e7c2c809942d37d118a2
Corey Owens <cowens@infinitecreations.io>
2025-04-05 21:12:52 -0500

    moved folder

---
+++ src/api/mobile-clinic/routes/mobile-clinic.ts
@@ -0,0 +1,7 @@
+/**
+ * mobile-clinic router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::mobile-clinic.mobile-clinic');
---
+++ src/api/mobile-clinic/services/mobile-clinic.ts
@@ -0,0 +1,7 @@
+/**
+ * mobile-clinic service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::mobile-clinic.mobile-clinic');
